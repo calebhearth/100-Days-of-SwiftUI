385f9115e4f69e62ecefbecbcad7b48c43ce609b
Caleb Hearth <caleb@calebhearth.com>
2024-03-14 10:46:22 -0600

    Checkpoint 1

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+let celciusTemp = -40.0
+let farenheitTemp = celciusTemp * 9 / 5 + 32
+print(celciusTemp)
+print(farenheitTemp)
+
+let anotherCelciusTemp = 22.0
+let anotherFarenheitTemp = anotherCelciusTemp * 9 / 5 + 32
