1fa3c3d7ca3b584af45efb4fb7b3674cdb0534d5
Caleb Hearth <caleb@calebhearth.com>
2024-03-18 12:39:22 -0600

    Checkpoint 2 / Day 4

---
+++ Checkpoint2.playground/Contents.swift
@@ -0,0 +1,20 @@
+// https://www.hackingwithswift.com/quick-start/beginners/checkpoint-2
+
+let characters = [
+  "Luke Skywalker",
+  "C-3PO",
+  "R2-D2",
+  "Darth Vader",
+  "Leia Organa",
+  "Owen Lars",
+  "Beru Whitesun lars",
+  "Leia Organa",
+  "Owen Lars",
+  "Beru Whitesun lars",
+  "R5-D4",
+  "Biggs Darklighter",
+  "Obi-Wan Kenobi"
+]
+
+print(characters.count) // => 13
+print(Set(characters).count) //=> 10
